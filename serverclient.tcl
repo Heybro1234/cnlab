4e99e96febd1ad3f49e606716f9010194c8f1d99
Hellobro125 <122431595+Hellobro125@users.noreply.github.com>
2023-01-11 21:00:53 +0530

    Update serverclient.tcl

--- serverclient.tcl
+++ serverclient.tcl
@@ -42,7 +42,23 @@ proc finish {} {
 
 }
 
+
 $ns at 0.01 "$ftp0 start"
 $ns at 15.0 "$ftp0 stop"
 $ns at 15.1 "finish"
 $ns run
+
+#BEGIN {
+#count=0;
+#time=0;
+#}
+#{if ( $1 == "r" && $4 == 1 && $5 == "tcp")
+#{
+#count += $6;
+#time=$2;
+#printf("\n%f\t%f",time,(count)/1000000);
+#}
+#}
+#END {
+#}
+
